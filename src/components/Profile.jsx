5dfb418ce5fedb3e9ce29524dfe3765bc080de22
OyaEr <56042138+OyaEr@users.noreply.github.com>
2021-04-14 22:35:51 +0300

    Profil arayüzü tamamlandı.

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,51 @@
 import React from "react";
+import { Form, Button, Card } from 'react-bootstrap'
 
 function Profile() {
     return (
-        <div>
-            <h1>Profile</h1>
-        </div>
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Profilim</h2>
+                    <Form.Group>
+                        <Form.Label>T.C. Kimlik No:</Form.Label>
+                        <Form.Control disabled={true} value={32132121} ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Email:</Form.Label>
+                        <Form.Control disabled={true} value={"dsasa@gmail.com"} ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>İsim:</Form.Label>
+                        <Form.Control value={1 } ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Soyad:</Form.Label>
+                        <Form.Control value={1 } ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Telefon Numarası::</Form.Label>
+                        <Form.Control value={1} ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Yakın kişinin ismi:</Form.Label>
+                        <Form.Control value={1} ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Yakın kişinin telefon numarası:</Form.Label>
+                        <Form.Control value={1} ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>2. Yakın kişinin ismi:</Form.Label>
+                        <Form.Control value={1} ></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>2. Yakın kişinin telefon numarası::</Form.Label>
+                        <Form.Control value={1} ></Form.Control>
+                    </Form.Group>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
 export default Profile;
