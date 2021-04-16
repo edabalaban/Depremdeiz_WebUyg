99446ab92da8444528ab056c5f564741ddfc2c89
edabalaban <edabalabannn@gmail.com>
2021-04-16 19:24:32 +0300

    map bug fixed

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -6,14 +6,13 @@ import MyHelps from "./Help/MyHelp"
 import OthersHelp from "./Help/OtherHelp"
 import HelpHeader from "./Help/HelpHeader"
 import CreateHelp from "./Help/CreateHelp/CreateNewHelp"
-import MeetOtherHelp from "./Help/MeetOtherHelps/MeetOtherHelp"
 
 var database = app.database();
 
 function Help() {
         useEffect(() => {
-        var helpRef  = database.ref('Help');
-        helpRef.on('value', (snapshot) => {
+        var starCountRef = database.ref('Help');
+        starCountRef.on('value', (snapshot) => {
             const data = snapshot.val();
             console.log(data);
         });
@@ -27,7 +26,6 @@ function Help() {
                     <Route path="/help/myhelps" component={MyHelps} />
                     <Route path="/help/otherhelps" component={OthersHelp} />
                     <Route path="/help/createHelp" component={CreateHelp} />
-                    <Route path="/help/meetotherhelp" component={MeetOtherHelp} />
                 </Switch>
             </Router> 
         </div>
