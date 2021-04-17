8bad1751eceede566987f37a3586e1d07e774438
cerenDabbagoglu <cdabbagoglu@etu.edu.tr>
2021-04-17 22:09:10 +0300

    ui updated

--- src/components/Help/OthersHelpRow.jsx
+++ src/components/Help/OthersHelpRow.jsx
@@ -16,18 +16,13 @@ function OthersHelpRow(props) {
 
     return (
         <div className="help" onClick={onClick}>
-            <h1>{props.helpKey}</h1>
-            <h1>{props.item.currentDate}</h1>
+            <h1 style={{color: "purple"}} >{props.item.currentDate}</h1>           
             {props.item.helpList.map((item, index) => {
-                return <h1 key={index} >{JSON.stringify(item.demandName)}</h1>
+              return (
+                <h1 style={item.isProvided ? { color: "green" } : { color: "red" }}  key={index} >{(item.demandName)}</h1>
+                )                
             })}
             <h1>Yardım talebinde bulunan: {props.item.isim}</h1>
-            <div>
-                {
-                    props.item.isCompletelyProvided ? <h1>Tamamen Karşılandı></h1> : <h1>Karşılanmadı</h1>
-                }
-            </div>
-
         </div>
     );
 }
